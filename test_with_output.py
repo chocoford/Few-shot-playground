2fbf7b01676bf5b29a11e169360666e21f9ece2d
Dove Zachary <504001583@qq.com>
2020-04-22 00:30:46 +0800

    test

--- test_with_output.py
+++ test_with_output.py
@@ -132,7 +132,7 @@ def main(_run, _config, _log):
             query_pred, _ = model(support_images, support_fg_mask, support_bg_mask,
                                   query_images)
             # _log.info(f'len of query_images: {len(query_images)}')
-            visualizer.saveImg(query_images[0], str(i))
+            visualizer.saveImgs(query_images[0], str(i))
                 
 
     #             metric.record(np.array(query_pred.argmax(dim=1)[0].cpu()),
