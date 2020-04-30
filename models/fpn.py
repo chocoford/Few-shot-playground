b84e0ff237af56b187c1b9251f83b026044f9261
Dove Zachary <504001583@qq.com>
2020-04-30 16:19:42 +0800

    test

--- models/fpn.py
+++ models/fpn.py
@@ -22,8 +22,8 @@ class _FPN(nn.Module):
     """ FPN """
     def __init__(self, classes, class_agnostic):
         super(_FPN, self).__init__()
-        self.classes = classes
-        self.n_classes = len(classes)
+        # self.classes = classes
+        # self.n_classes = len(classes)
         self.class_agnostic = class_agnostic
         # loss
         self.RCNN_loss_cls = 0
